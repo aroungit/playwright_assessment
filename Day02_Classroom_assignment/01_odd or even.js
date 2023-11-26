4376a7d843c4c4c4d05c299814aa846895bd879d
aroungit <151991298+aroungit@users.noreply.github.com>
2023-11-26 15:33:02 +0530

    Add files via upload

---
+++ Day02_Classroom_assignment/01_odd or even.js
@@ -0,0 +1,14 @@
+let value = 3;
+numbers(value)
+function numbers(value){  
+    
+    for (value = 1; value<=10; value++) {
+        if (value %2 != 0)
+        {
+            console.log (value + "")
+        }
+            
+        }
+       // console.log("the number is odd")
+    }
+
---
+++ Day02_Classroom_assignment/02_sum of values.js
@@ -0,0 +1,12 @@
+let val = 5;
+let b = 0;
+sum(val)
+function sum(val){
+    for (a=1; a<=5; a++){
+        b = b+a;
+        console.log (b);
+
+
+    }
+
+}
