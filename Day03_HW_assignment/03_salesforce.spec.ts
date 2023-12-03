04c82a5e717196f486413b6f2bff446544d53e8e
aroungit <151991298+aroungit@users.noreply.github.com>
2023-12-03 12:28:37 +0530

    Add files via upload

---
+++ Day03_HW_assignment/03_salesforce.spec.ts
@@ -0,0 +1,44 @@
+/*
+Assignment - 1 (Login to Salesforce)
+
+1) Open Salesforce Login Page https://login.salesforce.com
+2) Enter the username
+3) Enter the password
+4) Click Login
+5) Print the title and url
+*/
+
+
+import { chromium, test } from "@playwright/test";
+
+test("launch the salesforce application", async () => {
+
+    const browserInstance = await chromium.launch({ headless: false, channel: "chrome" });
+
+    const browserContext = await browserInstance.newContext();
+
+    const page = await browserContext.newPage();
+
+    await page.goto("https://login.salesforce.com");
+
+    await page.fill("#username", "pranavrajaroun@testleaf.com");
+
+    test.setTimeout(40000);
+
+    await page.fill("#password", "Automation@2023");
+
+    await page.locator('//input[@id="Login"]').click();
+
+    test.setTimeout(30000);
+
+    const title = await page.title();
+
+    console.log('the page title is + ${title}');
+
+    const url = page.url();
+
+    console.log('the page url is "+ ${url}');
+
+});
+
+
---
+++ Day03_HW_assignment/04_createnewlead.spec.ts
@@ -0,0 +1,47 @@
+/* Assignment - 2 (Create a new Lead)
+
+1) Open http://leaftaps.com/opentaps/control/main
+2) Perform the following functions
+
+    a) Login (Enter username, password, Click Login)
+    b) Click CRM/SFA Link (Locator for Link: text='Text of the Link') 
+       await page.locator("text=CRM/SFA').click();
+    c) Click the Leads Link (See the Tab)
+    d) Click Create Lead Link (See Left Menu)
+    e) Enter the company name, first name and last name (use id and pur your own data)
+    f) Click Create Lead Button 
+
+*/
+
+import { chromium, test } from "@playwright/test";
+
+test("create a new lead", async () => {
+    const browserInstance = await chromium.launch({ headless: false, channel: "chrome" });
+
+    const browserContext = await browserInstance.newContext();
+
+    const page = await browserContext.newPage();
+
+    await page.goto("http://leaftaps.com/opentaps/control/main");
+
+    await page.fill('#username', 'Demosalesmanager');
+
+    await page.fill('#password', 'crmsfa');
+
+    await page.locator('input[type=submit]').click();
+
+    await page.locator('text = CRM/SFA').click();
+
+    await page.locator('//a[contains(text(),"Leads")]').click();
+
+    await page.locator('//a[contains(text(),"Create Lead")]').click();
+
+    await page.fill('.inputBox', 'tcs');
+
+    await page.fill('#createLeadForm_firstNameLocal', 'aroun');
+
+    await page.fill('[name = lastNameLocal]', 'kumar');
+
+    await page.locator('[value="Create Lead"]').click();
+
+})
